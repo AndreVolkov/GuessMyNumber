8d85d5ea40961dfa4a34692e30fbe2c74db26619
Ande <a.volkov@lajm.lt>
2023-02-14 18:20:31 +0200

    fixed function. Added exit from the loop through a break

--- GuessMyNumber.py
+++ GuessMyNumber.py
@@ -1,5 +1,4 @@
 # Guess My Number
-import sys                                      
 from random import randint                       
 secret_value = randint(1,100)                              
 i = 0                                           
@@ -7,14 +6,15 @@ print(secret_value)
 def gmn(guess_value, secret_value):                                     
     if guess_value == secret_value:                                  
         print('Correct')                        
-        sys.exit()                              
+                                  
     elif secret_value > guess_value:                                 
         print('More')                           
     else:                                       
         print('Less')                           
-    return gmn                                  
 while i != 10:                                  
     i = i + 1                                    
     guess_value = int(input('Please enter your guess value:')) 
-    gmn(guess_value, secret_value)                                      
+    gmn(guess_value, secret_value)
+    if guess_value == secret_value:
+        break
 print('Game Over')                                  
